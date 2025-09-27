75e14e66c19b5f260f1d8c081f6fd0d0f4b47845
KrutikaChincholkar07 <chincholkarkrutika07@gmail.com>
2025-09-27 11:51:32 +0530

    Create HalfPatternR.java
    This Diamond Half Pattern Right code in Java
          *
        * *
      * * *
    * * * *
      * * *
        * *
          *

---
+++ HalfPatternR.java
@@ -0,0 +1,27 @@
+public class DiamondPattern {
+    public static void main(String[] args) {
+        int rows = 4;
+
+        // Upper half
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // Lower half
+        for (int i = rows - 1; i >= 1; i--) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
